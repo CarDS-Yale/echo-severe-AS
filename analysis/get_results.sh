d99f9ec2cddb1967540204acc1dcd4bd6eab5ddb
gholste <giholste@gmail.com>
2023-05-28 15:43:47 -0400

    Fix typo in main results .sh script.

--- analysis/get_results.sh
+++ analysis/get_results.sh
@@ -1,5 +1,5 @@
 for cohort in '051823_full_test_2016-2020' '100122_test_2021' 'cedars'; do
     for eval_type in 'studies' 'videos' 'one_video'; do
-        nohup python 052123_get_main_results.py --cohort "$cohort" --eval_type "$eval_type" --alpha 0.95 --n_samples 10000 &
+        nohup python get_results.py --cohort "$cohort" --eval_type "$eval_type" --alpha 0.95 --n_samples 10000 &
     done
 done
